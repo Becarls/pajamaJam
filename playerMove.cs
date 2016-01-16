6e7c4235008307c89683b30df5b4498ace4a92e8
E. Conrad Diao <ecdiao@umich.edu>
2016-01-16 00:01:33 -0500

    PlayerMove Edits

---
+++ playerMove.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+	public Rigidbody rigidCube;
+	public int playerPos = 1;
+	public float movementSpeed = 2;
+	public bool isMoving = false;
+	// Use this for initialization
+	void Start () {
+		rigidCube = this.GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isMoving) {
+		}else{
+			if(Input.GetKey(KeyCode.LeftArrow)){
+				playerPos--;
+				this.transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
+			}
+
+			if(Input.GetKey(KeyCode.RightArrow)){
+				playerPos++;
+				this.transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
+			}
+		}
+	}
+}
