ff7a24efaabb0090557dea7d3435e3cda8d7a0b4
Becarls <becarls@umich.edu>
2016-01-15 23:52:25 -0500

    hi

---
+++ Pajama Jam/Assets/_Scripts/Chords.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chords : MonoBehaviour {
+    
+    public float lifespan = 3f;
+    public float speed = 25f;
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(new Vector3(speed * -10f, 0, 0));
+        Destroy(this.gameObject, lifespan);
+	}
+	
+}
---
+++ Pajama Jam/Assets/_Scripts/shooting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class  shooting : MonoBehaviour {
+
+    public GameObject chord1;
+    /* public GameObject chord2;
+     public GameObject chord3;
+     public GameObject chord4;
+     public GameObject chord5;
+     public GameObject chord6;
+     public GameObject chord7;*/
+    public float timestamp;
+    public float delay;
+    private float tme;
+
+	void Start() {
+        tme = delay;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q)) {  
+            if (tme >= delay) {
+                Instantiate(chord1.transform, transform.position, Quaternion.Euler(0, 0, 90f));
+                tme = 0;
+            }
+        }
+        tme += Time.deltaTime;
+    }
+}
