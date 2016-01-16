3d9f6f9dc37f9300e79380b88766ae90e816f396
Becarls <becarls@umich.edu>
2016-01-16 01:22:00 -0500

    7 chords

--- Assets/_Scripts/shooting.cs
+++ Assets/_Scripts/shooting.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class  shooting : MonoBehaviour {
 
     public GameObject chord1;
-    /* public GameObject chord2;
+    public GameObject chord2;
      public GameObject chord3;
      public GameObject chord4;
      public GameObject chord5;
      public GameObject chord6;
-     public GameObject chord7;*/
+     public GameObject chord7;
     public float timestamp;
     public float delay;
     private float tme;
@@ -23,6 +23,42 @@ public class  shooting : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Q)) {  
             if (tme >= delay) {
                 Instantiate(chord1.transform, transform.position , Quaternion.Euler(0, 0, 90f));
+                tme = 0;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.W)) {
+            if (tme >= delay) {
+                Instantiate(chord2.transform, transform.position, Quaternion.Euler(0, 0, 90f));
+                tme = 0;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.E)) {
+            if (tme >= delay) {
+                Instantiate(chord3.transform, transform.position, Quaternion.Euler(0, 0, 90f));
+                tme = 0;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.R)) {
+            if (tme >= delay) {
+                Instantiate(chord4.transform, transform.position, Quaternion.Euler(0, 0, 90f));
+                tme = 0;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+            if (tme >= delay) {
+                Instantiate(chord5.transform, transform.position, Quaternion.Euler(0, 0, 90f));
+                tme = 0;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.F)) {
+            if (tme >= delay) {
+                Instantiate(chord6.transform, transform.position, Quaternion.Euler(0, 0, 90f));
+                tme = 0;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Space)) {
+            if (tme >= delay) {
+                Instantiate(chord7.transform, transform.position, Quaternion.Euler(0, 0, 90f));
                 tme = 0;
             }
         }
