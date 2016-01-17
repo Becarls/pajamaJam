22029787f41394870eb6ceb26be6ee71df4b555f
E. Conrad Diao <ecdiao@umich.edu>
2016-01-17 07:51:15 -0500

    Conrad Fatal 7:51

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+public class LoadOnClick : MonoBehaviour {
+
+	public void LoadScene(int level)
+	{
+		LoadScene (level);
+	}
+}
