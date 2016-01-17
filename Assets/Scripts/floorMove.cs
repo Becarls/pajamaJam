23a8786915333edb5a5735152f6c4fcec6051ae1
Becarls <becarls@umich.edu>
2016-01-17 04:41:38 -0500

    fuck this floor

---
+++ Assets/Scripts/floorMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floorMove : MonoBehaviour {
+
+    // Use this for initialization
+    public float lifespan = 3f;
+    public float speed = 250f;
+    // public ParticleSystem explosion;
+    void Start() { 
+        Destroy(gameObject, lifespan);
+    }
+
+    void Update() {
+        transform.Translate(speed * Time.deltaTime,0,0);
+    }
+}
